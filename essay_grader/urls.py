4452ab03520970cccdfbd75bd611708dd80fc7b5
surajkarki <suraj.karki500@protonmail.com>
2024-01-05 17:54:42 +0100

    make app for production

--- essay_grader/urls.py
+++ essay_grader/urls.py
@@ -1,33 +1,38 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
+from django.http import HttpResponseNotAllowed, JsonResponse
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 schema_view = get_schema_view(
-    openapi.Info(
-        title="Snippets API",
-        default_version='v1',
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
+   openapi.Info(
+      title="Freyja API",
+      default_version='v1',
+      description=""" This is a web app to grade essay using LSTM model using Django REST, React and Tensorflow. The dataset used to train the NLP model is https://www.kaggle.com/mpwolke/hewlett-foundation-essay-scoring """,
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="suraj.karki500@gmail.com"),
+      license=openapi.License(name="GPL V3 License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
 )
+
+def welcome_view(request):
+    if request.method == 'GET':
+        data = {'message': "Welcome to the Freyja's API!. To see the API documentation, go to http://127.0.0.1:8000/docs"}
+        return JsonResponse(data)
+    else:
+        data = {'error': 'Not Found'}
+        return HttpResponseNotAllowed(['GET'], content_type='application/json')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('api/', include('users.urls')),
-
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
-            schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger',
-                                               cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc',
-                                             cache_timeout=0), name='schema-redoc'),
-
+    path('api/user/', include('users.urls')),
+    path('', welcome_view, name="home"),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
