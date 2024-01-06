9968c49eb72ca674bb03d9e79441d90659980a87
surajkarki <suraj.karki500@protonmail.com>
2024-01-06 03:50:35 +0100

    Remove makemigration command

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
 echo "Collect Static..."
